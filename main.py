267d6dbe72d02756badb5ccced5dfa001051d280
Larrynuhuh <swastiksingh1402@gmail.com>
2026-06-11 22:11:48 +0530

    basic components working

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import jax
 import jax.numpy as jnp
 
+@jax.jit(static_argnames='terms')
+def solve(x, n, terms):
+    r = jnp.arange(1, terms+1)
+    coeff = (n - r + 1)/r
+    term = coeff * x
+
+    prod_term = jnp.concatenate([jnp.array([1]), jnp.cumprod(term)])
+    summed_term = jnp.sum(prod_term)
+
+    return summed_term
+
